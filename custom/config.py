1ab539fda2bb800dde3faf021a903a52c2d9eddb
Torkium <julien.taront@gmail.com>
2021-02-27 21:07:39 +0100

    Change base url

--- custom/config.py
+++ custom/config.py
@@ -5,7 +5,7 @@ class Config:
     """"Class containing configuration"""
 
     # API URL
-    API_BASE_URL = "http://localhost/kryll_backtests/api/"
+    API_BASE_URL = "https://api.backtest.kryll.torkium.com/"
     API_CHECK_BACKTEST_URL = API_BASE_URL+"index.php?controller=Backtest&action=check"
     API_GET_PERIOD_URL = API_BASE_URL+"index.php?controller=Pair&action=getPeriod"
     API_SEND_URL = API_BASE_URL+"index.php?controller=Backtest&action=send"
